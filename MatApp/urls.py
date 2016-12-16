da302c202c93fd47381a649e34eee9860c654f8f
Alexey-Petukhov <hertaras.hertaras@gmail.com>
2016-12-16 19:42:00 +0500

    add url for extra method

--- MatApp/urls.py
+++ MatApp/urls.py
@@ -4,6 +4,7 @@ from MatApp import views
 urlpatterns = [
     url(r'^$', views.CheckMat.home, name='home'),
     url(r'^check$', views.CheckMat.check, name='check'),
+    url(r'^check1$', views.CheckMat.check1, name='check1'),
     url(r'^addWord', views.AddNewWords.addWord, name='addWord'),
     url(r'^addDict', views.AddNewWords.jsonSave, name='addDict'),
     url(r'^addingWord', views.AddNewWords.addingWord, name='addingWord'),
