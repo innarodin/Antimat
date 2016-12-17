185fc425aea8a0c357f7bae681783f57e2603bcc
innarodin <innarodin@mail.ru>
2016-12-17 16:55:50 +0500

    Рабочая магия вставки слов

--- MatApp/models.py
+++ MatApp/models.py
@@ -2,5 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class Article(models.Model):
-    title = models.CharField(max_length=100)
--- MatApp/views.py
+++ MatApp/views.py
@@ -5,32 +5,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from djantimat.helpers import PymorphyProc
 import json
-
-class DBConnection: #singleton
-    instance = None
-    connection = None
-   # cursor = None
-
-    def __new__(cls):
-        print('in new')
-        if DBConnection.instance is None:
-            DBConnection.instance = object.__new__(cls)
-        return DBConnection.instance
-
-    def __init__(self):
-        print('in init')
-        if self.connection is None:
-            try:
-                self.connection = sqlite3.connect('db.sqlite3')
-                print('Database connection opened.')
-            except sqlite3.DatabaseError as db_error:
-                print("Error :\n{0}".format(db_error))
-
-    def __del__(self):
-        if self.connection is not None:
-            self.connection.close()
-            print('Database connection closed.')
-
+from djantimat.models import Slang
 
 class CheckMat:
     @staticmethod
@@ -79,29 +54,18 @@ class AddNewWords:
         with open('F:\\Users\\Admin\\Desktop\\file.json', encoding='utf-8') as data_file:
             data = json.loads(data_file.read())
         #data = json.loads('[{ "word": "\u0430\u0431\u043e\u0440\u0442\u043c\u0430\u0445\u0435\u0440", "pk": 1},{ "word": "\u0430\u043d\u0430\u043b", "pk": 2}]')
-        i = 0
-        while data[i + 1] != None:
+        for i in data:
             text = data[i]['word']
             AddNewWords.saveInDB(text)
-            i = i + 1
       #  conn.close()
         return render(request, "MatApp/home.html")
 
+
     @staticmethod
-    def saveInDB(text): #я хз почему не работает
-        print('In save')
-        conn = DBConnection()
-        print(conn)
+    def saveInDB(text):
+        b = Slang(word=text.lower())
+        b.save()
 
-        print("before cursor")
-        cursor = conn.connection.cursor()
-        print("after cursor")
-
-        cursor.execute('INSERT INTO djantimat_slang (word) VALUES (?)', (text,))
-        print("after insert")
-
-        conn.connection.commit()
-        print("after commit")
 
     @staticmethod
     def addWord(request):
