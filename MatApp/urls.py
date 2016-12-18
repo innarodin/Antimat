04f303b4edbf81d80fa45be1f8f613aed0518f06
Alexey-Petukhov <hertaras.hertaras@gmail.com>
2016-12-18 21:07:21 +0500

    добавлен url для методов по ajax

--- MatApp/urls.py
+++ MatApp/urls.py
@@ -4,9 +4,9 @@ from MatApp import views
 urlpatterns = [
     url(r'^$', views.CheckMat.home, name='home'),
     url(r'^check$', views.CheckMat.check, name='check'),
-    url(r'^check1$', views.CheckMat.check1, name='check1'),
+    url(r'^checkForAjax', views.CheckMat.checkForAjax, name='checkForAjax'),
     url(r'^addWord', views.AddNewWords.addWord, name='addWord'),
     url(r'^addDict', views.AddNewWords.jsonSave, name='addDict'),
     url(r'^addingWord', views.AddNewWords.addingWord, name='addingWord'),
-    url(r'^testFunc$', views.TestClass.testFunc, name='testFunc'),
+    url(r'^addWordForAjax', views.AddNewWords.addWordForAjax, name='addWordForAjax'),
 ]
